2794222dc34cf331e78dae4c792d8843dab3ac99
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2021-08-16 19:41:16 +0700

    Settings added

---
+++ src/clean_out_co/lib/pages/settings.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xffA2DEB8)
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10,
+                        left: 20
+                      ),
+                      child: Text(
+                        'Settings',
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontFamily: 'Abhaya Libre',
+                          fontWeight: FontWeight.w800
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1.5,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        top: 3
+                      ),
+                      child: Text(
+                        'Personalisasi',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Abhaya Libre'
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Color(0xff69B083),
+                      thickness: 1,
+                    ),
+                    SizedBox(height: 2,),
+                    SettingButton(
+                      icon: Icons.notifications_none,
+                      text: 'Notifikasi'
+                    ),
+                    SizedBox(height: 10,),
+                    SettingButton(
+                      icon: Icons.language,
+                      text: 'Bahasa',
+                    ),
+                    SizedBox(height: 50),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10,
+                          top: 3
+                      ),
+                      child: Text(
+                        'Personalisasi',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w800,
+                            fontFamily: 'Abhaya Libre'
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Color(0xff69B083),
+                      thickness: 1,
+                    ),
+                    SizedBox(height: 2,),
+                    SettingButton(
+                      icon: Icons.help_outline,
+                      text: 'Bantuan',
+                    ),
+                    SizedBox(height: 10,),
+                    SettingButton(
+                      icon: Icons.info_outline,
+                      text: 'Privacy Policy',
+                    ),
+                    SizedBox(height: 10,),
+                    SettingButton(
+                      icon: Icons.info_outline,
+                      text: 'Terms of Services',
+                    ),
+                    SizedBox(height: 10,),
+                    SettingButton(
+                      icon: Icons.star_border,
+                      text: 'Beri Kami Penilaian'
+                    ),
+                    SizedBox(height: 10,),
+                    SettingButton(
+                      icon: Icons.format_quote,
+                      text: 'Tentang CleanOut.Co',
+                    ),
+                  ],
+                ),
+              ),
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          )
+        ),
+      )
+    );
+  }
+}
+
+class SettingButton extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  final Function()? onPressed;
+
+  const SettingButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+    this.onPressed,
+  }) : super(key: key);
+
+
+  @override
+  _SettingButtonState createState() => _SettingButtonState();
+}
+
+class _SettingButtonState extends State<SettingButton> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xff91D8AB),
+            shadowColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0)
+            )
+          ),
+          onPressed: widget.onPressed,
+          child: Container(
+            width: double.infinity,
+            height: 60,
+            padding: EdgeInsets.only(
+              left: 10,
+              top: 5,
+              bottom: 5,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Color(0xff026928),
+                  child: Icon(
+                    widget.icon,
+                    color: Colors.white,
+                  )
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Text(
+                    widget.text,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.navigate_next_rounded,
+                  color: Color(0xff69B083),
+                  size: 48,
+                )
+              ],
+            ),
+          )
+        ),
+        Divider(
+          height: 0,
+          thickness: 1,
+          color: Color(0xff69B083),
+        )
+      ],
+    );
+  }
+}
